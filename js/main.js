a1764c768a57202217d955211ece54f9fa15f795
Rosie L <40253226+rosielash@users.noreply.github.com>
2018-12-05 11:41:59 -0600

    Added Services Section
    Added Services Section

---
+++ js/main.js
@@ -0,0 +1,21 @@
+
+	
+// Nav Function//
+	
+	function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+	
+// END Nav Function//		
+		
+
+		
+  $('.mini.modal')
+  .modal('show')
+; 
+   
